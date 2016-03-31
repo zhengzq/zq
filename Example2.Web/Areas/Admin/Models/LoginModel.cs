dcb65c1f3aa5047ebfa791847dae7714c87ac549
郑志强 <4062499@qq.com>
2016-03-31 23:03:56 +0800

    LoginModel

---
+++ Example2.Web/Areas/Admin/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Example2.Web.Areas.Admin.Models
+{
+    public class LoginModel
+    {
+        [DisplayName("登入账号")]
+        [Required(ErrorMessage = "请输入账号")]
+        public string LoginName { get; set; }
+        [DisplayName("密码")]
+        [Required(ErrorMessage = "请输入密码")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+        public string Error { get; set; }
+    }
+}
